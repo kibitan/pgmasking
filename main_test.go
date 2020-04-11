4d26867aff33821771376fb9a0c19cb9c01e5e7c
kibitan <uzukifirst@gmail.com>
2020-04-11 10:58:20 +0200

    write integration test

--- main_test.go
+++ main_test.go
@@ -1,7 +1,38 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+var (
+	r *os.File
+	w *os.File
+)
+
+func TestMain(m *testing.M) {
+	// faking Stdin/stdout
+	s := os.Stdout
+	r, w, _ = os.Pipe()
+	os.Stdout = w
+
+	code := m.Run()
+
+	os.Stdout = s
+	os.Exit(code)
+}
 
 func TestCallMain(t *testing.T) {
-	// TODO: write integration test?
+	want := "pgmasking test sql\ntest test"
+	w.Write([]byte(want))
+
+	main()
+
+	w.Close()
+	got, _ := ioutil.ReadAll(r)
+
+	if string(got) != want {
+		t.Errorf("Do: got (%s), want (%s)", string(got), want)
+	}
 }
