79ba9b1ead70835eeca395414d193fa1dd32e890
kibitan <uzukifirst@gmail.com>
2020-04-11 10:38:39 +0200

    use bufio instead of ioutil reader

--- inout/inout.go
+++ inout/inout.go
@@ -2,11 +2,11 @@
 package inout
 
 import (
+	"bufio"
 	"io"
-	"io/ioutil"
 )
 
 func Do(i io.Reader, o io.Writer) {
-	b, _ := ioutil.ReadAll(i) // should refactoring with efficient buffer
-	o.Write(b)
+	r := bufio.NewReader(i)
+	r.WriteTo(o)
 }
