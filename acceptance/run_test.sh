9f0ffddf4dd45f7d6e885314401b63917bcc1ce3
kibitan <uzukifirst@gmail.com>
2020-04-13 21:50:20 +0200

    change directory

--- acceptance/run_test.sh
+++ acceptance/run_test.sh
@@ -22,7 +22,7 @@ if (_psql -c '\dt' | grep users) > /dev/null 2>&1; then
 fi
 
 ## import database
-_psql < "$FILEDIR/$POSTGRES_VERSION/import_dumpfile.sql" > /dev/null
+_psql < "$FILEDIR/import_dumpfile/$POSTGRES_VERSION.sql" > /dev/null
 
 ## pgmasking
 PGPASSWORD="$POSTGRES_PASSWORD" pg_dump --no-owner -h "$POSTGRES_HOST" -U "$POSTGRES_USER" "$POSTGRES_DBNAME" | pgmasking -c "$FILEDIR/masking.yml" > "$FILEDIR/tmp/pgmasking_dumpfile.sql"
