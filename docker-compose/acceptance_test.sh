9f7924df33475341c41c7e454bd888b50983d85c
kibitan <uzukifirst@gmail.com>
2020-04-13 12:49:49 +0200

    add docker-compose test set

---
+++ docker-compose/acceptance_test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -Ceu -o pipefail
+set -vx # for debug
+
+POSTGRES_VERSION=${1:-postgres12}
+DOCKER_COMPOSE_FILE=${2:-docker-compose.yml}
+docker-compose -f "$DOCKER_COMPOSE_FILE" -f "docker-compose/$POSTGRES_VERSION.yml" run -e "POSTGRES_HOST=$POSTGRES_VERSION" app acceptance/run_test.sh
