e72a303709a11eaac8165b3e8f451e44d4da49b2
kibitan <uzukifirst@gmail.com>
2020-04-15 17:55:32 +0200

    WIP

--- inout/inout.go
+++ inout/inout.go
@@ -8,7 +8,8 @@ import (
 )
 
 // Do will read lines from input and process it and write it to output
-func Do(i io.Reader, o io.Writer) {
+func Do(i io.Reader, o io.Writer, p func(*string) *string) error {
 	r := bufio.NewReader(i)
 	r.WriteTo(o)
+	return nil
 }
--- inout/inout_test.go
+++ inout/inout_test.go
@@ -11,11 +11,27 @@ func TestInout(t *testing.T) {
 
 	i := strings.NewReader("abcdef\n123455")
 	o := new(bytes.Buffer)
-	Do(i, o)
+	p := func(s *string) *string { return s }
+	err := Do(i, o, p)
 
 	if o.String() != want {
 		t.Errorf("Do: got (%s), want (%s)", o.String(), want)
 	}
 
-	// TODO: write error scenario
+	if err != nil {
+		t.Errorf("Do returns error: %s", err)
+	}
+
+	want = "ABCDEF\n123455"
+	p = func(s *string) *string { return &strings.ToUpper(*s) }
+	err = Do(i, o, p)
+
+	if o.String() != want {
+		t.Errorf("Do: got (%s), want (%s)", o.String(), want)
+	}
+
+	if err != nil {
+		t.Errorf("Do returns error: %s", err)
+	}
+
 }
